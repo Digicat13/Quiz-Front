84b3d068c689763e1c2a2be2b7e13b858173c8db
Maryna Diuh <mdiuh@leobit.com>
2020-12-28 10:12:01 +0200

    Jwt-interceptor

---
+++ src/app/services/authentication-service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { IUser } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<IUser>;
+  public currentUser: Observable<IUser>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<IUser>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): IUser {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string): Observable<Object> {
+    return this.http
+      .post(`${environment.apiUrl}/account/signin`, { username, password })
+      .pipe(
+        map((user) => {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
