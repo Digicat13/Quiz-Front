676aa5153e10813d4bdbfea0702eb038655151e1
Maryna Diuh <mdiuh@leobit.com>
2020-12-30 20:27:20 +0200

    added validation on first test create step

--- src/app/components/create-test/create-test.component.ts
+++ src/app/components/create-test/create-test.component.ts
@@ -29,13 +29,14 @@ export class CreateTestComponent implements OnInit {
   createTestForm = new FormGroup({
     name: new FormControl(null, [Validators.required]),
     description: new FormControl(null, [Validators.required]),
+    timeOption: new FormControl(null, Validators.required),
     testTimeLimit: new FormGroup({
       hours: new FormControl(null, [Validators.pattern('^[0-9]$')]),
-      minutes: new FormControl(null, [Validators.pattern('^[0-5]d$')]),
+      minutes: new FormControl(null, [Validators.pattern('^[0-5][0-9]$')]),
     }),
     questionTimeLimit: new FormGroup({
-      minutes: new FormControl(null, [Validators.pattern('^d{2}$')]),
-      seconds: new FormControl(null, [Validators.pattern('^[0-5]d$')]),
+      minutes: new FormControl(null, [Validators.pattern('[0-9]{0,2}')]),
+      seconds: new FormControl(null, [Validators.pattern('[0-5]?[0-9]')]),
     }),
   });
 
@@ -49,6 +50,33 @@ export class CreateTestComponent implements OnInit {
       'questionTimeLimit'
     ).value;
 
+    if (this.createTestForm.invalid) {
+      return;
+    }
+
+    if (this.createTestForm.get('timeOption').value === 'testTime') {
+      const hours = this.createTestForm.get('testTimeLimit.hours').value;
+      const minutes = this.createTestForm.get('testTimeLimit.minutes').value;
+      if (hours || minutes) {
+        this.test.testTimeLimit.hours = hours ? hours : 0;
+        this.test.testTimeLimit.minutes = minutes ? minutes : 0;
+      } else {
+        return;
+      }
+    }
+
+    if (this.createTestForm.get('timeOption').value === 'questionTime') {
+      const minutes = this.createTestForm.get('questionTimeLimit.minutes')
+        .value;
+      const seconds = this.createTestForm.get('questionTimeLimit.seconds')
+        .value;
+      if (minutes || seconds) {
+        this.test.questionTimeLimit.minutes = minutes ? minutes : 0;
+        this.test.questionTimeLimit.seconds = minutes ? minutes : 0;
+      } else {
+        return;
+      }
+    }
     this.stepOneSubmit.emit(this.test);
   }
 }
--- src/app/models/test.ts
+++ src/app/models/test.ts
@@ -1,11 +1,12 @@
 import { Time } from '@angular/common';
 import { IQuestion } from './question';
+import { ITime } from './time';
 
 export interface ITest {
     id?: string;
     name: string;
     description: string;
     testTimeLimit: Time;
-    questionTimeLimit: Time;
+    questionTimeLimit: ITime;
     questions: Array<IQuestion>;
 }
