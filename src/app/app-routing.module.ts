bf82531ab9373707b52b248c8fdadb7d19f61bb1
Maryna Diuh <37348506+Digicat13@users.noreply.github.com>
2021-01-06 12:26:53 +0200

    Add login page (#5)
    * added folders to project
    
    * created basic structure for home-page
    
    * create basic card-list with test names and description
    
    * Created navbar
    
    * Created basic system to add and delete questions and answers
    
    * before changing to FormBuilder
    
    * unsuccessful
    
    * added stepper
    
    * navigation
    
    * Add create-test-page
    
    * Add route
    
    * login form
    
    * added login route
    
    * removed @media from scss
    
    Co-authored-by: Maryna Diuh <mdiuh@leobit.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddQuestionComponent } from './components/add-question/add-question.component';
 import { CreateTestPageComponent } from './pages/create-test-page/create-test-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'login-page', component: LoginPageComponent },
   { path: 'home-page', component: HomePageComponent },
   { path: 'add-question', component: AddQuestionComponent },
   { path: 'create-test', component: CreateTestPageComponent },
