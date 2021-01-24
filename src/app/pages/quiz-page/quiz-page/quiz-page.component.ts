5b26c589a45941d9b50764b21e4accff1ff768a8
Maryna Diuh <mdiuh@leobit.com>
2021-01-24 18:36:02 +0200

    quiz-page

--- src/app/pages/quiz-page/quiz-page/quiz-page.component.ts
+++ src/app/pages/quiz-page/quiz-page/quiz-page.component.ts
@@ -12,6 +12,7 @@ import { ITestingResultAnswer } from 'src/app/models/testingResultAnswer';
 import * as moment from 'moment';
 import { TestingResultService } from 'src/app/services/testingResult.service';
 import { Subject } from 'rxjs';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-page',
@@ -30,12 +31,23 @@ export class QuizPageComponent implements OnInit {
   testingAnswers: ITestingResultAnswer[] = new Array<ITestingResultAnswer>();
   onNextQuestion: Subject<void> = new Subject<void>();
   onEndQuiz: Subject<void> = new Subject<void>();
+  testForm: FormGroup = this.fb.group({
+    id: [],
+    name: [],
+    description: [],
+    testTimeLimit: [],
+    questionTimeLimit: [],
+    questions: this.fb.array([]),
+    // answersArray: this.fb.array([]),
+    // correctAnswersCount: [0]
+  });
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private testService: TestService,
     private testingService: TestingService,
-    private testingResultService: TestingResultService
+    private testingResultService: TestingResultService,
+    private fb: FormBuilder
   ) {
     this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
       this.testingId = params.get('id');
@@ -65,6 +77,7 @@ export class QuizPageComponent implements OnInit {
     this.testService.getTest(testId).subscribe(
       (test: ITest) => {
         this.test = test;
+        this.InitTestForm();
       },
       (error) => {
         console.log('Failed to retrieve test');
@@ -72,9 +85,50 @@ export class QuizPageComponent implements OnInit {
     );
   }
 
+  InitTestForm(): void {
+    this.testForm.setValue({
+      id: this.test.id,
+      name: this.test.name,
+      description: this.test.description,
+      testTimeLimit: this.test.testTimeLimit,
+      questionTimeLimit: this.test.questionTimeLimit,
+      questions: [],
+    });
+
+    const control = this.testForm.controls.questions as FormArray;
+    this.test.questions.forEach((question: IQuestion) => {
+      control.push(
+        this.fb.group({
+          id: question.id,
+          testId: question.testId,
+          questionText: question.questionText,
+          hintText: question.hintText,
+          correctAnswersCount: question.answers.filter(
+            (answer: IAnswer) => answer.isCorrect === true
+          ).length,
+          answers: this.CreateTestAnswers(question.answers),
+        })
+      );
+    });
+  }
+
+  CreateTestAnswers(answers: IAnswer[]): FormArray {
+    const array = new FormArray([]);
+    answers.forEach((answer: IAnswer) => {
+      array.push(
+        this.fb.group({
+          id: answer.id,
+          selected: false,
+          answerText: [answer.answerText, []],
+        })
+      );
+    });
+    return array;
+  }
+
   nextQuestion(): void {
-    this.getAnswers();
-    this.currentQuestionSelectedAnswers = [];
+    // this.getAnswers();
+    // this.currentQuestionSelectedAnswers = [];
 
     if (this.currentQuestionIndex !== this.test?.questions?.length - 1) {
       this.currentQuestionIndex++;
@@ -83,28 +137,48 @@ export class QuizPageComponent implements OnInit {
     this.onNextQuestion.next();
   }
 
-  onSelectAnswer(selectedAnswers): void {
-    this.currentQuestionSelectedAnswers = lodash.cloneDeep(selectedAnswers);
-  }
+  // getAnswers(): void {
+  //   if (this.answeredQuestions?.length === 0) {
+  //     this.initAnsweredQuestionsArray();
+  //   }
 
-  initAnsweredQuestionsArray(): void {
-    this.answeredQuestions = lodash.cloneDeep(this.test.questions);
-    this.answeredQuestions.forEach((question) => {
-      question.answers = [];
-    });
-  }
+  //   this.answeredQuestions[this.currentQuestionIndex].answers = [
+  //     ...this.currentQuestionSelectedAnswers,
+  //   ];
+
+  //   this.currentQuestionSelectedAnswers.forEach((answer) => {
+  //     this.testingAnswers.push({
+  //       testAnswerId: answer.id,
+  //       testQuestionId: answer.testQuestionId,
+  //     });
+  //   });
+  // }
+
+  // onSelectAnswer(selectedAnswers): void {
+  //   this.currentQuestionSelectedAnswers = lodash.cloneDeep(selectedAnswers);
+  // }
+
+  // initAnsweredQuestionsArray(): void {
+  //   this.answeredQuestions = lodash.cloneDeep(this.test.questions);
+  //   this.answeredQuestions.forEach((question) => {
+  //     question.answers = [];
+  //   });
+  // }
 
   onSubmit(quizDurationSeconds: number): void {
-    this.getAnswers();
+    // this.getAnswers();
+
+    console.log(this.testForm);
+
     const testingResult: ITesting = this.getTestingResult(quizDurationSeconds);
-    this.testingResultService.createTestingResult(testingResult).subscribe(
-      (result) => {
-        console.log(result);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    // this.testingResultService.createTestingResult(testingResult).subscribe(
+    //   (result) => {
+    //     console.log(result);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
   }
 
   startQuiz(interviewee: string): void {
@@ -113,36 +187,32 @@ export class QuizPageComponent implements OnInit {
     }
 
     this.currentQuestionIndex = 0;
-    this.testingStartDateTime = new Date();
+    this.testingStartDateTime = moment().toDate();
   }
 
   getTestingResult(quizDurationSeconds: number): ITestingResult {
     const testingResult: ITestingResult = {};
     testingResult.testingId = this.testingId;
     testingResult.intervieweeName = this.testing.intervieweeName;
-    testingResult.selectedAnswers = this.testingAnswers;
     testingResult.testingStartDateTime = this.testingStartDateTime;
     testingResult.duration = moment.utc(
       moment.duration({ seconds: quizDurationSeconds }).asMilliseconds()
     );
-    return testingResult;
-  }
-
-  getAnswers(): void {
-    if (this.answeredQuestions?.length === 0) {
-      this.initAnsweredQuestionsArray();
-    }
-
-    this.answeredQuestions[this.currentQuestionIndex].answers = [
-      ...this.currentQuestionSelectedAnswers,
-    ];
-
-    this.currentQuestionSelectedAnswers.forEach((answer) => {
-      this.testingAnswers.push({
-        testAnswerId: answer.id,
-        testQuestionId: answer.testQuestionId,
+    testingResult.selectedAnswers = new Array<ITestingResultAnswer>();
+    console.log(testingResult.selectedAnswers);
+    this.testForm.value.questions.forEach((question) => {
+      question.answers.forEach((answer) => {
+        if (answer.selected === true) {
+          testingResult.selectedAnswers?.push({
+            testQuestionId: question.id,
+            testAnswerId: answer.id,
+          });
+        }
       });
     });
+    console.log(testingResult.selectedAnswers);
+    console.log(testingResult);
+    return testingResult;
   }
 
   endQuiz(): void {
